6a0c147318eb4a62ce92a392d2e15e15f550eaf9
K7kstn ICT team <ict@kessetsu.net>
2022-02-13 04:13:10 +0900

    GHP Team C :  暗闇避難アプリ
      SOS 連絡ボード 組み込み
    
    　　試行 :  サイドパネル表示  ケース 2
    　　（サイドパネルを全部は引っ込めないケース）
    
       Reference: https://bootstrapious.com/p/bootstrap-sidebar#sidebar-4
    
         #4 Advanced sidebar のケース

--- js/sos-msg-ctrl.js
+++ js/sos-msg-ctrl.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
 
-    $('.sosBadge').on('click', function () {
+    // $('.sosBadge').on('click', function () {
+    $('#sos-msg-sidepanelCollapse').on('click', function () {
         $('#sos-msg-sidepanel').toggleClass('active');
     });
 
