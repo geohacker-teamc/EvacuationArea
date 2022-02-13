024d2bf0086b7fd70760df4e52dbf1b8b0eaad4b
K7kstn ICT team <ict@kessetsu.net>
2022-02-13 15:42:44 +0900

    GHP2021 Team C :  夜間集団避難たすけあい アプリ
      - SOS コミュニケーション用 サイドパネル 調整
    
       https://goworkship.com/magazine/slide-out-sidebars/
    
        Bootstrap 4.5 に サイドバー 制御がなかった (Bootstrap-Vue にはあり)
        ため 上記サイトの
    
    　　6. Responsive sidebar  by Antonija Šimic を参考に対応。

--- js/sos-msg-ctrl.js
+++ js/sos-msg-ctrl.js
@@ -1,9 +1,32 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-    // $('.sosBadge').on('click', function () {
-    $('#sos-msg-sidepanelCollapse').on('click', function () {
-        $('#sos-msg-sidepanel').toggleClass('active');
-    });
+  function toggleSidebar() {
+    // $("#sos-msg-sidepanelCollapse").toggleClass("active");  // UI: button or badge
+    $("#content").toggleClass("move-to-right");
+    // $(".sidebar-item").toggleClass("active");
+  }
+
+  /*
+  function toggleSidebarClose() {
+    $("#content").toggleClass("move-to-left");
+  }
+  */
+
+  // UI: button or badge manipulation
+  $("#sos-msg-sidepanelCollapse").on("click tap", function() {
+    toggleSidebar();
+  });
+
+  // UI: button or badge manipulation
+  $(".sos-msg-sidepanel-close-btn").on("click tap", function() {
+    toggleSidebar();
+  });
+
+  $(document).keyup(function(e) {
+    if (e.keyCode === 27) {
+      toggleSidebar();
+    }
+  });
 
 });
 
